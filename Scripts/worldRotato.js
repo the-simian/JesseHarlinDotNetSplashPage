ca8fcc96ebd16784f32f52a2de0c3fd2f044bcce
Jesse Harlin <harlinjesse@gmail.com>
2013-02-12 20:56:21 -0600

    slight change

--- Scripts/worldRotato.js
+++ Scripts/worldRotato.js
@@ -38,9 +38,9 @@ $(document).ready(function () {
     var distanceFromCamera = 10;
 
 
-    var numberOfObjects = 7;
+    var numberOfObjects = 20;
 
-    var numberOfSublayers = 4;
+    var numberOfSublayers = 3;
 
     var objectLayers = [];
     //**-------------
@@ -83,10 +83,12 @@ $(document).ready(function () {
         };        
 
 
-        var wAndH = randomNumber(50, 200);
+        var wAndH = randomNumber(25, 300);
         var subtractor = randomNumber(1, 10) * numberOfSublayers;
 
-        for (var i = 0; i < numberOfSublayers ; i++) {
+        var subs = Math.ceil(randomNumber(0, numberOfSublayers));
+ 
+        for (var i = 0; i < subs ; i++) {
 
 
             wAndH = wAndH - subtractor;
@@ -94,12 +96,12 @@ $(document).ready(function () {
 
 
             var transData = {
-                x: randomNumber(-10, 10),
-                y: randomNumber(-10, 10),
-                z: randomNumber(-10, 10),
+                x: randomNumber(-30, 30),
+                y: randomNumber(-30, 30),
+                z: randomNumber(-30, 30),
                 rotZ: randomNumber(1, 90),
                 scale: randomNumber(0.3, 1),
-                speed: .1 * Math.random()
+                speed:   Math.random() * 0.7
             };
 
             
@@ -162,32 +164,31 @@ $(document).ready(function () {
             var oldData = $thisLayer.data('transform');
 
             var newData = {
-                x: oldData.x,
-                y: oldData.y,
-                z: oldData.z,
-                rotY: -worldYAngle,
-                rotX: -worldXAngle,
-                rotZ: oldData.rotZ,
-                scale: oldData.scale
+                x: oldData.x ,
+                y: oldData.y ,
+                z: oldData.z ,
+                rotY: -worldYAngle ,
+                rotX: -worldXAngle ,
+                rotZ: oldData.rotZ + oldData.speed,
+                scale: oldData.scale,
+                opacity: 1,
+                speed: oldData.speed
             };
 
+            
+
+
             $thisLayer.css({
-
                 'transform':
-
-                        
                     'translateX( ' + newData.x + 'px ) ' +
                     'translateY( ' + newData.y + 'px ) ' +   
                     'translateZ( ' + newData.z + 'px ) ' +
-                        
-
                     'rotateY( ' + newData.rotY + 'deg ) ' +
-                    'rotateX( ' + newData.rotX + 'deg ) ' 
-
-                    //'rotateZ( ' + newData.rotZ + 'deg ) ' +
-
-                        
-                    //'scale( '+ newData.scale+' )'
+                    'rotateX( ' + newData.rotX + 'deg ) ' +
+                    'rotateZ( ' + newData.rotZ + 'deg ) ' + 
+                    'scale( ' + newData.scale + ' )',
+                
+                'opacity': newData.opacity
                 
             }).data('transform', newData);
             
